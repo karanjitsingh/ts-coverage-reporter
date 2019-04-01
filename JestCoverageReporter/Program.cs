b20306fe11492590a7f07d057ecdea92c79e517a
Karan Jit Singh <karsin@microsoft.com>
2019-04-01 21:12:45 +0530

    1.0.1

--- JestCoverageReporter/Program.cs
+++ JestCoverageReporter/Program.cs
@@ -16,13 +16,16 @@ namespace JestCoverageReporter
         {
             var collectionUri = Environment.GetEnvironmentVariable("System.TeamFoundationCollectionUri");
             var projectId = Environment.GetEnvironmentVariable("System.TeamProjectId");
-            var buildId = int.Parse(Environment.GetEnvironmentVariable("Build.BuildId"));
+            int.TryParse(Environment.GetEnvironmentVariable("Build.BuildId"), out int buildId);
             var token = Environment.GetEnvironmentVariable("SYSTEM_ACCESSTOKEN");
 
             var coverageDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-            var mergeFile = Path.Combine(coverageDir, Guid.NewGuid().ToString(), ".coverage.json");
+            var mergeFile = Path.Combine(coverageDir, Guid.NewGuid().ToString() + ".coverage.json");
 
             Directory.CreateDirectory(coverageDir);
+
+            MergeCoverageFiles(args.ToList(), mergeFile);
+            Console.WriteLine(mergeFile);
 
             UploadCoverageFile.UploadCodeCoverageAttachmentsToNewStore(buildId, projectId, new List<string>() { mergeFile }, collectionUri, token);
         }
